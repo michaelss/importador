aecb8a3ce9248fb3d316f791455c52d669f93969
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2017-05-29 16:01:29 -0300

    Cleaning up, better formatting, translation to English.

--- src/main/java/br/jus/treto/importador/App.java
+++ src/main/java/br/jus/treto/importador/App.java
@@ -1,99 +1,49 @@
 package br.jus.treto.importador;
 
-import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Date;
-import java.util.List;
-import java.util.Properties;
-
-import oracle.jdbc.OracleConnection;
 import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class App {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		System.out.println("-------------------------------------------------------");
-		System.out.println(" Utilitário de importação de dados");
-		System.out.println("-------------------------------------------------------");
-		
-//		if (args.length != 1 && args.length != 7) {
-//			System.out.println("Faltam parâmetros");
-//			System.out.println("Usos possíveis: ");
-//			System.out.println("1> java -jar <arquivo>.jar <arquivo_com_configuracoes>");
-//			System.out.println(
-//					"2> java -jar <arquivo>.jar <host> <SID> <usuario> <senha> <tabela> <coluna> <arquivo_destino>");
-//			System.out.println(
-//					"Exemplo: java -jar extrator.jar maquina1 xe usuario1 senha123 produtos preco /tmp/relatorio.xml");
-//			System.out.printf("\nO arquivo com as configurações deve estar no formato 'chave=valor', como a seguir:\n");
-//			System.out.println("host=maquina1");
-//			System.out.println("sid=xe");
-//			System.out.println("usuario=usuario1");
-//			System.out.println("senha=senha123");
-//			System.out.println("tabela=produtos");
-//			System.out.println("coluna=preco");
-//			System.out.println("arquivo=/tmp/resultado.txt");
-//			return;
-//		}
+        System.out.println("-------------------------------------------------------");
+        System.out.println(" Utility to import CSV files");
+        System.out.println("-------------------------------------------------------");
 
-		Properties props;
-//		if (args.length == 1) {
-//			props = lerArqConfiguracao(args);
-//		} else {
-			props = new Properties();
-			props.put("host", args[0]);
-			props.put("sid", args[1]);
-			props.put("usuario", args[2]);
-			props.put("senha", args[3]);
-//			props.put("tabela", args[4]);
-//			props.put("coluna", args[5]);
-//			props.put("arquivo", args[6]);
-//		}
+        if (args.length != 1 && args.length != 5) {
+            System.out.println("Parameters missing");
+            System.out.println("Usage: ");
+            System.out.println("java -jar <file>.jar <host> <SID> <user> <password> <filePath>");
+            System.out.println("Example: java -jar importador.jar host1 xe user1 pass123 /tmp/data.csv");
+            System.out.println("The file content must be like \"data\";\"data\"");
+            return;
+        }
 
-        props.put("arquivo", args[4]);
+        new App().processFile(args[0], args[1], args[2], args[3], args[4]);
+    }
 
-        App acesso = new App();
-		acesso.lerArquivo(props, acesso);
-	}
+    private void processFile(final String host, final String sid, final String user, final String password,
+                             final String filePath) throws IOException {
 
-	private void lerArquivo(final Properties props, App acesso) throws IOException {
-		final String nomeArquivo = props.getProperty("arquivo");
-
-        final File f = new File(nomeArquivo);
-        final List<String> lines = FileUtils.readLines(f, "UTF-8");
+        final File file = new File(filePath);
+        final List<String> lines = FileUtils.readLines(file, "UTF-8");
 
         final int numThreads = 50;
         final int sublinhas = lines.size() / numThreads;
-        System.out.println("Sublinhas: " + sublinhas);
 
-        System.out.println("Iniciou... " + new Date().toString());
+        System.out.println("Started at " + new Date().toString());
 
         for (int i = 0; i < numThreads; i++) {
-
-                new Insersor(i+1, props.getProperty("host"),
-                        props.getProperty("sid"),
-                        props.getProperty("usuario"),
-                        props.getProperty("senha"),
-                        lines.subList(i * sublinhas, ((i + 1) * sublinhas))).start();
-
-//            System.out.printf("%d - %d \n", i * sublinhas, ((i + 1) * sublinhas));
-
+            new Insersor(host, sid, user, password, lines.subList(i * sublinhas, ((i + 1) * sublinhas))).start();
         }
 
         int resto = lines.size() % numThreads;
-
         if (resto != 0) {
-//            System.out.printf("%d - %d \n", lines.size() - resto, lines.size());
-            new Insersor(numThreads, props.getProperty("host"),
-                    props.getProperty("sid"),
-                    props.getProperty("usuario"),
-                    props.getProperty("senha"),
-                    lines.subList(lines.size() - resto, lines.size())).start();
+            new Insersor(host, sid, user, password, lines.subList(lines.size() - resto, lines.size())).start();
         }
     }
 
